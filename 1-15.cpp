d305ab6ba3765d360517c22d90fec2db27f698a0
qkd0863 <qkd7183@naver.com>
2023-10-18 01:16:31 +0900

    1-14 ~ 1-15 1-16 미완성
    vertex 수정

---
+++ 1-15.cpp
@@ -0,0 +1,600 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include<cstdlib>
+#include<ctime>
+#include <gl/glew.h> //--- 필요한 헤더파일 include
+#include<gl/freeglut.h>
+#include <gl/freeglut_ext.h>
+#include <gl/glm/glm.hpp>
+#include <gl/glm/ext.hpp>
+#include <gl/glm/gtc/matrix_transform.hpp>
+
+using namespace std;
+
+
+
+GLvoid drawScene(GLvoid);
+GLvoid Reshape(int w, int h);
+GLvoid Keyboard(unsigned char key, int x, int y);
+void Mouse(int button, int state, int x, int y);
+void Drag(int x, int y);
+void TimerFunction(int value);
+GLvoid Special(int key, int x, int y);
+void convert(int x, int y, float* ox, float* oy);
+void InitBuffer();
+void make_vertexShaders();
+void make_fragmentShaders();
+void make_shaderProgram();
+GLvoid drawScene();
+GLvoid Reshape(int w, int h);
+char* filetobuf(const char* file);
+void set();
+
+
+double x1[5], Y1[5], z1[5];
+float mx = 0, my = 0;
+bool left_button;
+bool c1 = false, c2 = false, c3 = false, c4 = false, rect = false;
+float X = 30, Y = -30;
+float movex = 0, movey = 0;
+bool xrot = false, yrot = false;
+bool grain = true;
+bool wire = false;
+bool Hex = true;
+
+
+GLfloat HexahedronShape[36][3];
+GLfloat HexahedronColors[36][3];
+GLfloat TetrahedronShape[18][3];
+GLfloat TetrahedronColors[18][3];
+
+
+
+GLuint HexPosVbo, TetPosVbo;
+GLuint HexColor, TetColor;
+
+
+GLint width, height;
+GLuint shaderProgramID; //--- 세이더 프로그램 이름
+GLuint vertexShader; //--- 버텍스 세이더 객체
+GLuint fragmentShader; //--- 프래그먼트 세이더 객체
+
+
+
+
+
+
+
+void main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정 
+{
+	set();
+	//--- 윈도우 생성하기
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(800, 600);
+	glutCreateWindow("Example1");
+	//--- GLEW 초기화하기
+	glewExperimental = GL_TRUE;
+	glewInit();
+	make_shaderProgram();
+	InitBuffer();
+	glutMouseFunc(Mouse);
+	glutMotionFunc(Drag);
+	glutKeyboardFunc(Keyboard);
+	glutDisplayFunc(drawScene);
+	glutReshapeFunc(Reshape);
+	glutSpecialFunc(Special);
+	glutTimerFunc(100, TimerFunction, 1);
+	
+	glutMainLoop();
+}
+
+GLvoid drawScene() //--- 콜백 함수: 그리기 콜백 함수
+{
+	GLfloat rColor, gColor, bColor;
+	rColor = gColor = 0.0;
+	bColor = 0.0;
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	
+	if (grain)
+		glEnable(GL_DEPTH_TEST);
+	else
+		glDisable(GL_DEPTH_TEST);
+
+	glUseProgram(shaderProgramID);
+
+	
+
+
+
+	int PosLocation = glGetAttribLocation(shaderProgramID, "in_Position"); //	: 0  Shader의 'layout (location = 0)' 부분
+	int ColorLocation = glGetAttribLocation(shaderProgramID, "in_Color"); //	: 1
+
+	glEnableVertexAttribArray(PosLocation); // Enable 필수! 사용하겠단 의미
+	glEnableVertexAttribArray(ColorLocation);
+
+	
+
+
+
+	glm::mat4 a = glm::mat4(1.0f);
+	glm::mat4 model = glm::mat4(1.0f);
+	glm::mat4 model2 = glm::mat4(1.0f);
+	glm::mat4 model3 = glm::mat4(1.0f);
+	glm::mat4 model4 = glm::mat4(1.0f);
+	glm::mat4 model5 = glm::mat4(1.0f);
+	GLuint changePos = glGetUniformLocation(shaderProgramID, "Pos");
+
+
+
+	model = glm::rotate(model, glm::radians(X), glm::vec3(1.0, 0.0, 0.0));
+	model2 = glm::rotate(model2, glm::radians(Y), glm::vec3(0.0, 1.0, 0.0));
+	model3 = glm::translate(model3, glm::vec3(movex, movey, 0.0f));
+
+	model4 = model3* model * model2 ;
+
+
+
+	unsigned int modelLocation = glGetUniformLocation(shaderProgramID, "transform");
+	glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model4));
+
+
+
+	if (Hex)
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, HexPosVbo); // VBO Bind
+		glVertexAttribPointer(PosLocation, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+		{
+			glBindBuffer(GL_ARRAY_BUFFER, HexColor); // VBO Bind
+			glVertexAttribPointer(ColorLocation, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+		}
+
+
+		if (wire)
+			glDrawArrays(GL_LINE_STRIP, 0, 36);
+		else
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+
+	}
+	else
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, TetPosVbo); // VBO Bind
+		glVertexAttribPointer(PosLocation, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+		{
+			glBindBuffer(GL_ARRAY_BUFFER, TetColor); // VBO Bind
+			glVertexAttribPointer(ColorLocation, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+		}
+
+
+		if (wire)
+			glDrawArrays(GL_LINE_STRIP, 0, 18);
+		else
+			glDrawArrays(GL_TRIANGLES, 0, 18);
+	}
+	
+
+
+
+
+	
+
+	
+
+
+	
+	
+
+
+
+	glDisableVertexAttribArray(PosLocation); // Disable 필수!
+	glDisableVertexAttribArray(ColorLocation);
+
+	
+
+	glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(a));
+	glLineWidth(5.0);
+	
+	glBegin(GL_LINES);
+	glColor3f(0.0, 1.0, 0.0);
+	glVertex3f(-1.0, 0.0, 0.0);
+	glVertex3f(1.0, 0.0, 0.0);
+	glVertex3f(0.0, -1.0, 0.0);
+	glVertex3f(0.0, 1.0, 0.0);
+	glEnd();
+	glFinish();
+
+	glutSwapBuffers();
+	glutPostRedisplay();
+
+	
+}
+
+
+GLvoid Reshape(int w, int h) //--- 콜백 함수: 다시 그리기 콜백 함수
+{
+	glViewport(0, 0, w, h);
+}
+
+GLvoid Keyboard(unsigned char key, int x, int y)
+{
+	srand(unsigned(time(NULL)));
+
+	switch (key)
+	{
+	case GLUT_KEY_LEFT:
+		movex += 0.01;
+		break;
+	case 'x':
+		xrot = true;
+		yrot = false;
+		break;
+	case 'y':
+		xrot = false;
+		yrot = true;
+		break;
+	case 'h':
+		if (grain)
+			grain = false;
+		else
+			grain = true;
+		break;
+	case 'w':
+		if (wire)
+			wire = false;
+		else
+			wire = true;
+		break;
+	case 's':
+		xrot = false;yrot = false;
+		X = 30;Y = -30;
+		movex = 0; movey = 0;
+		break;
+	case 'c':
+		Hex = true;
+		break;
+	case 'p':
+		Hex = false;
+		break;
+	case 'q':
+		exit(1);
+		break;
+	}
+
+
+	glutPostRedisplay();
+}
+GLvoid Special(int key, int x, int y)
+{
+	switch (key)
+	{
+	case GLUT_KEY_LEFT:
+		movex -= 0.01;
+		break;
+	case GLUT_KEY_RIGHT:
+		movex += 0.01;
+		break;
+	case GLUT_KEY_UP:
+		movey += 0.01;
+		break;
+	case GLUT_KEY_DOWN:
+		movey -= 0.01;
+		break;
+	}
+
+	glutPostRedisplay();
+}
+
+void Mouse(int button, int state, int x, int y)
+{
+	srand((unsigned int)time(NULL));
+
+	float Rx, Ry, Wx, Wy;
+
+
+	convert(x, y, &Wx, &Wy);
+	convert(x, y, &Rx, &Ry);
+
+
+
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		
+	}
+
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_UP)
+	{
+	
+	}
+}
+
+void Drag(int x, int y)
+{
+	float Rx, Ry, Wx, Wy;
+
+	convert(x, y, &Wx, &Wy);
+	convert(x, y, &Rx, &Ry);
+
+	
+
+
+	glutPostRedisplay();
+}
+
+
+
+
+
+void convert(int x, int y, float* ox, float* oy)
+{
+	int w = 800;
+	int h = 600;
+	*ox = (float)(x - (float)w / 2.0) * (float)(1.0 / (float)(w / 2.0));
+	*oy = -(float)(y - (float)h / 2.0) * (float)(1.0 / (float)(h / 2.0));
+}
+
+
+
+
+void TimerFunction(int value)
+{
+	if(xrot)
+		X += 3;
+	if (yrot)
+		Y += 3;
+
+	glutPostRedisplay(); // 화면 재 출력
+	glutTimerFunc(100, TimerFunction, 1); // 타이머함수 재 설정
+}
+
+
+GLchar* vertexSource, * fragmentSource; //--- 소스코드 저장 변수
+
+void make_vertexShaders()
+{
+	vertexSource = filetobuf("vertex.glsl");
+	//--- 버텍스 세이더 객체 만들기
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	//--- 세이더 코드를 세이더 객체에 넣기
+	glShaderSource(vertexShader, 1, (const GLchar**)&vertexSource, 0);
+	//--- 버텍스 세이더 컴파일하기
+	glCompileShader(vertexShader);
+	//--- 컴파일이 제대로 되지 않은 경우: 에러 체크
+	GLint result;
+	GLchar errorLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, errorLog);
+		cerr << "ERROR: vertex shader 컴파일 실패\n" << errorLog << endl;
+		return;
+	}
+}
+
+void make_fragmentShaders()
+{
+	fragmentSource = filetobuf("fragment.glsl");
+	//--- 프래그먼트 세이더 객체 만들기
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	//--- 세이더 코드를 세이더 객체에 넣기
+	glShaderSource(fragmentShader, 1, (const GLchar**)&fragmentSource, 0);
+	//--- 프래그먼트 세이더 컴파일
+	glCompileShader(fragmentShader);
+	//--- 컴파일이 제대로 되지 않은 경우: 컴파일 에러 체크
+	GLint result;
+	GLchar errorLog[512];
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, errorLog);
+		cerr << "ERROR: fragment shader 컴파일 실패\n" << errorLog << endl;
+		return;
+	}
+}
+
+
+void make_shaderProgram()
+{
+	make_vertexShaders(); //--- 버텍스 세이더 만들기
+	make_fragmentShaders(); //--- 프래그먼트 세이더 만들기
+	//-- shader Program
+	shaderProgramID = glCreateProgram();
+	glAttachShader(shaderProgramID, vertexShader);
+	glAttachShader(shaderProgramID, fragmentShader);
+	glLinkProgram(shaderProgramID);
+	//--- 세이더 삭제하기
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	//--- Shader Program 사용하기
+	glUseProgram(shaderProgramID);
+}
+
+
+
+char* filetobuf(const char* file)
+{
+	FILE* fptr;
+	long length;
+	char* buf;
+	fptr = fopen(file, "rb"); // Open file for reading
+	if (!fptr) // Return NULL on failure
+		return NULL;
+	fseek(fptr, 0, SEEK_END); // Seek to the end of the file
+	length = ftell(fptr); // Find out how many bytes into the file we are
+	buf = (char*)malloc(length + 1); // Allocate a buffer for the entire length of the file and a null terminator
+	fseek(fptr, 0, SEEK_SET); // Go back to the beginning of the file
+	fread(buf, length, 1, fptr); // Read the contents of the file in to the buffer
+	fclose(fptr); // Close the file
+	buf[length] = 0; // Null terminator
+	return buf; // Return the buffer
+}
+
+
+
+void InitBuffer()
+{
+	glGenBuffers(1, &HexPosVbo);
+	glBindBuffer(GL_ARRAY_BUFFER, HexPosVbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(HexahedronShape), HexahedronShape, GL_DYNAMIC_DRAW);
+	glGenBuffers(1, &HexColor);
+	glBindBuffer(GL_ARRAY_BUFFER, HexColor);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(HexahedronColors), HexahedronColors, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &TetPosVbo);
+	glBindBuffer(GL_ARRAY_BUFFER, TetPosVbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(TetrahedronShape), TetrahedronShape, GL_DYNAMIC_DRAW);
+	glGenBuffers(1, &TetColor);
+	glBindBuffer(GL_ARRAY_BUFFER, TetColor);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(TetrahedronColors), TetrahedronColors, GL_STATIC_DRAW);
+
+}
+
+
+void set()
+{
+	for (int i = 0;i < 3;i++)
+	{
+		TetrahedronColors[i][0] = 1;
+		TetrahedronColors[i][1] = 0;
+		TetrahedronColors[i][2] = 0;
+	}
+	for (int i = 3;i < 6;i++)
+	{
+		TetrahedronColors[i][0] = 0;
+		TetrahedronColors[i][1] = 1;
+		TetrahedronColors[i][2] = 0;
+	}
+	for (int i = 6;i < 9;i++)
+	{
+		TetrahedronColors[i][0] = 0;
+		TetrahedronColors[i][1] = 0;
+		TetrahedronColors[i][2] = 1;
+	}
+	for (int i = 9;i < 12;i++)
+	{
+		TetrahedronColors[i][0] = 1;
+		TetrahedronColors[i][1] = 1;
+		TetrahedronColors[i][2] = 0;
+	}
+	for (int i = 12;i < 15;i++)
+	{
+		TetrahedronColors[i][0] = 1;
+		TetrahedronColors[i][1] = 0;
+		TetrahedronColors[i][2] = 1;
+	}
+
+
+	for (int i = 0;i < 6;i++)
+	{
+		HexahedronColors[i][0] = 1;
+		HexahedronColors[i][1] = 0;
+		HexahedronColors[i][2] = 0;
+	}
+	for (int i = 6;i < 12;i++)
+	{
+		HexahedronColors[i][0] = 0;
+		HexahedronColors[i][1] = 1;
+		HexahedronColors[i][2] = 0;
+	}
+	for (int i = 12;i < 18;i++)
+	{
+		HexahedronColors[i][0] = 0;
+		HexahedronColors[i][1] = 0;
+		HexahedronColors[i][2] = 1;
+	}
+	for (int i = 18;i < 24;i++)
+	{
+		HexahedronColors[i][0] = 1;
+		HexahedronColors[i][1] = 1;
+		HexahedronColors[i][2] = 0;
+	}
+	for (int i = 24;i < 30;i++)
+	{
+		HexahedronColors[i][0] = 0;
+		HexahedronColors[i][1] = 1;
+		HexahedronColors[i][2] = 1;
+	}
+	for (int i = 30;i < 36;i++)
+	{
+		HexahedronColors[i][0] = 1;
+		HexahedronColors[i][1] = 0;
+		HexahedronColors[i][2] = 1;
+	}
+	HexahedronShape[0][0] = 0.0;HexahedronShape[0][1] = 0.0;HexahedronShape[0][2] = 0.0;
+	HexahedronShape[1][0] = 0.5;HexahedronShape[1][1] = 0.5;HexahedronShape[1][2] = 0.0;
+	HexahedronShape[2][0] = 0.5;HexahedronShape[2][1] = 0.0;HexahedronShape[2][2] = 0.0;
+
+	HexahedronShape[3][0] = 0.0;HexahedronShape[3][1] = 0.0;HexahedronShape[3][2] = 0.0;
+	HexahedronShape[4][0] = 0.0;HexahedronShape[4][1] = 0.5;HexahedronShape[4][2] = 0.0;
+	HexahedronShape[5][0] = 0.5;HexahedronShape[5][1] = 0.5;HexahedronShape[5][2] = 0.0;
+
+	HexahedronShape[6][0] = 0.0;HexahedronShape[6][1] = 0.0;HexahedronShape[6][2] = 0.0;
+	HexahedronShape[7][0] = 0.0;HexahedronShape[7][1] = 0.5;HexahedronShape[7][2] = 0.5;
+	HexahedronShape[8][0] = 0.0;HexahedronShape[8][1] = 0.5;HexahedronShape[8][2] = 0.0;
+
+	HexahedronShape[9][0] = 0.0;HexahedronShape[9][1] = 0.0;HexahedronShape[9][2] = 0.0;
+	HexahedronShape[10][0] = 0.0;HexahedronShape[10][1] = 0.0;HexahedronShape[10][2] = 0.5;
+	HexahedronShape[11][0] = 0.0;HexahedronShape[11][1] = 0.5;HexahedronShape[11][2] = 0.5;
+
+	HexahedronShape[12][0] = 0.0;HexahedronShape[12][1] = 0.5;HexahedronShape[12][2] = 0.0;
+	HexahedronShape[13][0] = 0.5;HexahedronShape[13][1] = 0.5;HexahedronShape[13][2] = 0.5;
+	HexahedronShape[14][0] = 0.5;HexahedronShape[14][1] = 0.5;HexahedronShape[14][2] = 0.0;
+
+	HexahedronShape[15][0] = 0.0;HexahedronShape[15][1] = 0.5;HexahedronShape[15][2] = 0.0;
+	HexahedronShape[16][0] = 0.0;HexahedronShape[16][1] = 0.5;HexahedronShape[16][2] = 0.5;
+	HexahedronShape[17][0] = 0.5;HexahedronShape[17][1] = 0.5;HexahedronShape[17][2] = 0.5;
+
+	HexahedronShape[18][0] = 0.5;HexahedronShape[18][1] = 0.0;HexahedronShape[18][2] = 0.0;
+	HexahedronShape[19][0] = 0.5;HexahedronShape[19][1] = 0.5;HexahedronShape[19][2] = 0.0;
+	HexahedronShape[20][0] = 0.5;HexahedronShape[20][1] = 0.5;HexahedronShape[20][2] = 0.5;
+
+	HexahedronShape[21][0] = 0.5;HexahedronShape[21][1] = 0.0;HexahedronShape[21][2] = 0.0;
+	HexahedronShape[22][0] = 0.5;HexahedronShape[22][1] = 0.5;HexahedronShape[22][2] = 0.5;
+	HexahedronShape[23][0] = 0.5;HexahedronShape[23][1] = 0.0;HexahedronShape[23][2] = 0.5;
+
+	HexahedronShape[24][0] = 0.0;HexahedronShape[24][1] = 0.0;HexahedronShape[24][2] = 0.0;
+	HexahedronShape[25][0] = 0.5;HexahedronShape[25][1] = 0.0;HexahedronShape[25][2] = 0.0;
+	HexahedronShape[26][0] = 0.5;HexahedronShape[26][1] = 0.0;HexahedronShape[26][2] = 0.5;
+
+	HexahedronShape[27][0] = 0.0;HexahedronShape[27][1] = 0.0;HexahedronShape[27][2] = 0.0;
+	HexahedronShape[28][0] = 0.5;HexahedronShape[28][1] = 0.0;HexahedronShape[28][2] = 0.5;
+	HexahedronShape[29][0] = 0.0;HexahedronShape[29][1] = 0.0;HexahedronShape[29][2] = 0.5;
+
+	HexahedronShape[30][0] = 0.0;HexahedronShape[30][1] = 0.0;HexahedronShape[30][2] = 0.5;
+	HexahedronShape[31][0] = 0.5;HexahedronShape[31][1] = 0.0;HexahedronShape[31][2] = 0.5;
+	HexahedronShape[32][0] = 0.5;HexahedronShape[32][1] = 0.5;HexahedronShape[32][2] = 0.5;
+
+	HexahedronShape[33][0] = 0.0;HexahedronShape[33][1] = 0.0;HexahedronShape[33][2] = 0.5;
+	HexahedronShape[34][0] = 0.5;HexahedronShape[34][1] = 0.5;HexahedronShape[34][2] = 0.5;
+	HexahedronShape[35][0] = 0.0;HexahedronShape[35][1] = 0.5;HexahedronShape[35][2] = 0.5;
+
+
+	TetrahedronShape[0][0] = 0.0;TetrahedronShape[0][1] = 0.0;TetrahedronShape[0][2] = 0.0;
+	TetrahedronShape[1][0] = 0.5;TetrahedronShape[1][1] = 0.0;TetrahedronShape[1][2] = 0.0;
+	TetrahedronShape[2][0] = 0.0;TetrahedronShape[2][1] = 0.0;TetrahedronShape[2][2] = 0.5;
+
+	TetrahedronShape[3][0] = 0.5;TetrahedronShape[3][1] = 0.0;TetrahedronShape[3][2] = 0.5;
+	TetrahedronShape[4][0] = 0.5;TetrahedronShape[4][1] = 0.0;TetrahedronShape[4][2] = 0.0;
+	TetrahedronShape[5][0] = 0.0;TetrahedronShape[5][1] = 0.0;TetrahedronShape[5][2] = 0.5;
+
+	TetrahedronShape[6][0] = 0.5;TetrahedronShape[6][1] = 0.0;TetrahedronShape[6][2] = 0.0;
+	TetrahedronShape[7][0] = 0.0;TetrahedronShape[7][1] = 0.0;TetrahedronShape[7][2] = 0.0;
+	TetrahedronShape[8][0] = 0.25;TetrahedronShape[8][1] = 0.4;TetrahedronShape[8][2] = 0.25;
+
+	TetrahedronShape[9][0] = 0.0;TetrahedronShape[9][1] = 0.0;TetrahedronShape[9][2] = 0.5;
+	TetrahedronShape[10][0] = 0.0;TetrahedronShape[10][1] = 0.0;TetrahedronShape[10][2] = 0.0;
+	TetrahedronShape[11][0] = 0.25;TetrahedronShape[11][1] = 0.4;TetrahedronShape[11][2] = 0.25;
+
+	TetrahedronShape[12][0] = 0.5;TetrahedronShape[12][1] = 0.0;TetrahedronShape[12][2] = 0.0;
+	TetrahedronShape[13][0] = 0.5;TetrahedronShape[13][1] = 0.0;TetrahedronShape[13][2] = 0.5;
+	TetrahedronShape[14][0] = 0.25;TetrahedronShape[14][1] = 0.4;TetrahedronShape[14][2] = 0.25;
+
+	TetrahedronShape[15][0] = 0.0;TetrahedronShape[15][1] = 0.0;TetrahedronShape[15][2] = 0.5;
+	TetrahedronShape[16][0] = 0.5;TetrahedronShape[16][1] = 0.0;TetrahedronShape[16][2] = 0.5;
+	TetrahedronShape[17][0] = 0.25;TetrahedronShape[17][1] = 0.4;TetrahedronShape[17][2] = 0.25;
+}
