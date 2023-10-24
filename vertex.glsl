7bd20712adba7c1ac09861e0b5ec4068a8e87bf9
qkd0863 <qkd7183@naver.com>
2023-10-24 22:45:18 +0900

    vertex

--- vertex.glsl
+++ vertex.glsl
@@ -4,11 +4,14 @@
 layout (location = 0) in vec3 in_Position; //--- 위치 변수: attribute position 0
 layout (location = 1) in vec3 in_Color; //--- 컬러 변수: attribute position 1
 out vec3 out_Color; //--- 프래그먼트 세이더에게 전달
-uniform vec2 Pos;
+uniform vec3 Pos;
 uniform mat4 transform;
+uniform mat4 viewTransform;
+uniform mat4 projectionTransform;
+
 
 void main(void)
 {
-gl_Position = transform*vec4 (in_Position.x+Pos.x, in_Position.y+Pos.y, in_Position.z, 1.0);
+gl_Position =    projectionTransform*viewTransform*transform*vec4 (in_Position.x+Pos.x, in_Position.y+Pos.y, in_Position.z+Pos.z, 1.0);
 out_Color = in_Color;
 }
