fe9f00d7c3d2b085a1b61d9f14a79ef4a4947e10
n <n@gmail.com>
2023-11-22 11:15:53 +0900

    법선벡터 변환

--- vertex2.glsl
+++ vertex2.glsl
@@ -25,5 +25,7 @@ gl_Position =    projectionTransform*viewTransform*transform*vec4 (in_Position.x
 
 FragPos = vec3(transform * vec4(in_Position, 1.0)); //--- 객체에 대한 조명 계산을 프래그먼트 셰이더에서 한다. 
 //--- 따라서 월드공간에 있는 버텍스 값을 프래그먼트 셰이더로 보낸다.
-Normal = vNormal; //--- 노멀값을 프래그먼트 세이더로 보낸다.
+// Normal = vNormal; //--- 노멀값을 프래그먼트 세이더로 보낸다.
+
+	Normal = vec3(transform * vec4(vNormal, 1.0));
 }
