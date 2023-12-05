9d3bde4b5e0d02604f63115f2a966c44bf88ecf1
qkd0863 <qkd7183@naver.com>
2023-12-06 05:09:33 +0900

    1-30

--- fragment2.glsl
+++ fragment2.glsl
@@ -3,8 +3,9 @@
 
 in vec3 FragPos; //--- 위치값
 in vec3 Normal; //--- 버텍스 세이더에서 받은 노멀값
+in vec2 TexCoord;
 
-
+uniform sampler2D outTexture; 
 
 out vec4 FragColor; //--- 색상 출력
 
@@ -13,11 +14,11 @@ uniform vec3 lightColor; //--- 응용 프로그램에서 설정한 조명 색상
 uniform vec3 lightPos; //--- 조명의 위치
 uniform vec3 objectColor; //--- 응용 프로그램에서 설정한 객체의 색상
 uniform vec3 viewPos; //--- 응용 프로그램에서 설정한 객체의 색상
-
+uniform vec2 lightoff;
 
 void main ()
 {
-float ambientLight = 0.5; //--- 주변 조명 계수: 0.0 ≤ ambientLight ≤ 1.0
+float ambientLight = 0.5+lightoff.x; //--- 주변 조명 계수: 0.0 ≤ ambientLight ≤ 1.0
 vec3 ambient = ambientLight * lightColor; //--- 주변 조명값
 
 
@@ -42,5 +43,9 @@ vec3 specular = specularLight * lightColor;
 
 
 vec3 result = (ambient + diffuse + specular) * objectColor; //--- 최종 조명 설정된 픽셀 색상=(주변조명+산란반사조명)*객체 색상
+
+
+
 FragColor = vec4 (result, 1.0);
+FragColor = texture(outTexture, TexCoord) * FragColor;
 } 
--- vertex2.glsl
+++ vertex2.glsl
@@ -5,9 +5,12 @@
 
 layout (location = 0) in vec3 in_Position; //--- 위치 변수: attribute position 0
 layout (location = 1) in vec3 vNormal; //--- 컬러 변수: attribute position 1
+layout (location = 2) in vec2 vTexCoord;
 
 out vec3 FragPos; //--- 객체의 위치값을 프래그먼트 세이더로 보낸다.
 out vec3 Normal; //--- 노멀값을 프래그먼트 세이더로 보낸다.
+out vec2 TexCoord;
+
 
 out vec3 out_Color; //--- 프래그먼트 세이더에게 전달
 
@@ -26,6 +29,7 @@ gl_Position =    projectionTransform*viewTransform*transform*vec4 (in_Position.x
 FragPos = vec3(transform * vec4(in_Position, 1.0)); //--- 객체에 대한 조명 계산을 프래그먼트 셰이더에서 한다. 
 //--- 따라서 월드공간에 있는 버텍스 값을 프래그먼트 셰이더로 보낸다.
 // Normal = vNormal; //--- 노멀값을 프래그먼트 세이더로 보낸다.
-
+  
 	Normal = vec3(transform * vec4(vNormal, 1.0));
+	TexCoord = vTexCoord;
 }
